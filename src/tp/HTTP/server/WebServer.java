0aaee283cb885786d6f29742cf8bd2afb1a77180
ericu07 <57630135+ericu07@users.noreply.github.com>
2020-11-18 12:15:08 +0100

    head, get ~post

--- src/tp/HTTP/server/WebServer.java
+++ src/tp/HTTP/server/WebServer.java
@@ -2,84 +2,140 @@
 
 package tp.HTTP.server;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URI;
+import java.util.LinkedList;
 
 /**
  * Example program from Chapter 1 Programming Spiders, Bots and Aggregators in
  * Java Copyright 2001 by Jeff Heaton
- * 
+ * <p>
  * WebServer is a very simple web-server. Any request is responded with a very
  * simple web-page.
- * 
+ *
  * @author Jeff Heaton
  * @version 1.0
  */
 public class WebServer {
 
-  /**
-   * WebServer constructor.
-   */
-  protected void start() {
-    ServerSocket s;
-
-    System.out.println("Webserver starting up on port 80");
-    System.out.println("(press ctrl-c to exit)");
-    try {
-      // create the main server socket
-      s = new ServerSocket(3000);
-    } catch (Exception e) {
-      System.out.println("Error: " + e);
-      return;
+    /**
+     * Start the application.
+     *
+     * @param args Command line parameters are not used.
+     */
+    public static void main(String[] args) {
+        WebServer ws = new WebServer();
+        ws.start();
     }
 
-    System.out.println("Waiting for connection");
-    for (;;) {
-      try {
-        // wait for a connection
-        Socket remote = s.accept();
-        // remote is now the connected socket
-        System.out.println("Connection, sending data.");
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-            remote.getInputStream()));
-        PrintWriter out = new PrintWriter(remote.getOutputStream());
+    /**
+     * WebServer constructor.
+     */
+    protected void start() {
+        ServerSocket s;
 
-        // read the data sent. We basically ignore it,
-        // stop reading once a blank line is hit. This
-        // blank line signals the end of the client HTTP
-        // headers.
-        String str = ".";
-        while (str != null && !str.equals(""))
-          str = in.readLine();
+        System.out.println("Webserver starting up on port 3000");
+        System.out.println("(press ctrl-c to exit)");
+        try {
+            // create the main server socket
+            s = new ServerSocket(3000);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            return;
+        }
 
-        // Send the response
-        // Send the headers
-        out.println("HTTP/1.0 200 OK");
-        out.println("Content-Type: text/html");
-        out.println("Server: Bot");
-        // this blank line signals the end of the headers
-        out.println("");
-        // Send the HTML page
-        out.println("<H1>Welcome to the Ultra Mini-WebServer</H2>");
-        out.flush();
-        remote.close();
-      } catch (Exception e) {
-        System.out.println("Error: " + e);
-      }
+        System.out.println("Waiting for connection");
+        for (; ; ) {
+            try {
+                // wait for a connection
+                Socket remote = s.accept();
+                // remote is now the connected socket
+                System.out.println("Connection, sending data.");
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        remote.getInputStream()));
+                PrintStream out = new PrintStream(remote.getOutputStream());
+
+                // read the data sent. We basically ignore it,
+                // stop reading once a blank line is hit. This
+                // blank line signals the end of the client HTTP
+                // headers.
+                LinkedList<String> strs = new LinkedList<>();
+                String str;
+                do {
+                    System.out.print("read : ");
+                    str = in.readLine();
+                    strs.addLast(str);
+                    System.out.println(str);
+                } while (str != null && !str.equals(""));
+
+                String[] split = strs.get(0).split(" ");
+                System.out.println(split[0]);
+                switch (split[0]) {
+                    case "HEAD":
+                    case "GET":
+                    case "POST": {
+                        // File
+                        boolean error404 = false;
+                        String resource = split[1];
+                        File f = new File("resources/" + resource);
+                        if (!f.exists()) {
+                            System.out.println("404: " + f.getAbsolutePath());
+                            error404 = true;
+                            f = new File("resources/404.html");
+                        }
+
+                        // Send the response
+
+                        // Send the headers
+                        out.println(error404 ? "HTTP/1.0 404 NOT FOUND" : "HTTP/1.0 200 OK");
+                        out.println("Content-Type: text/html");
+                        out.println("Server: Bot");
+                        // this blank line signals the end of the headers
+                        out.println();
+
+                        // Send the HTML page
+                        if (!split[0].equals("HEAD")) {
+                            FileReader fr = new FileReader(f);
+                            BufferedReader br = new BufferedReader(fr);
+
+                            String line;
+                            while((line=br.readLine())!=null) {
+                                out.println(line);
+                            }
+                            out.println();
+                            out.flush();
+
+                            br.close();
+                            fr.close();
+                        }
+
+                        break;
+                    }
+                    default: {
+                        // Send the response
+
+                        // Send the headers
+                        out.println("HTTP/1.0 200 OK");
+                        out.println("Content-Type: text/html");
+                        out.println("Server: Bot");
+                        // this blank line signals the end of the headers
+                        out.println();
+
+                        // Send the HTML page
+                        out.println("<H1>Welcome to the Ultra Mini-WebServer</H1>");
+
+                        out.flush();
+                        break;
+                    }
+                }
+
+
+                //remote.close();
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
+        }
     }
-  }
-
-  /**
-   * Start the application.
-   * 
-   * @param args
-   *            Command line parameters are not used.
-   */
-  public static void main(String args[]) {
-    WebServer ws = new WebServer();
-    ws.start();
-  }
 }
