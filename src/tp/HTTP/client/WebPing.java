b7bdbc4a967a658865b2d1acd42ca91d17b5b635
AureDau <57732337+AureDau@users.noreply.github.com>
2020-11-18 10:48:34 +0100

    http

---
+++ src/tp/HTTP/client/WebPing.java
@@ -0,0 +1,36 @@
+package tp.HTTP.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class WebPing {
+  public static void main(String[] args) {
+  
+      if (args.length != 2) {
+      	System.err.println("Usage java WebPing <server host name> <server port number>");
+      	return;
+      }	
+  
+   String httpServerHost = args[0];
+    int httpServerPort = Integer.parseInt(args[1]);
+     httpServerHost = args[0];
+      httpServerPort = Integer.parseInt(args[1]);
+
+    try {
+      InetAddress addr;      Socket sock = new Socket(httpServerHost, httpServerPort);
+      addr = sock.getInetAddress();
+      System.out.println("Connected to " + addr);
+      BufferedReader in = new BufferedReader(new InputStreamReader(
+                sock.getInputStream()));
+      while(true) {
+       System.out.println(in.readLine());
+      }
+      //sock.close();
+    } catch (java.io.IOException e) {
+      System.out.println("Can't connect to " + httpServerHost + ":" + httpServerPort);
+      System.out.println(e);
+    }
+  }
+}
